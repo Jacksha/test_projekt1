f3d7f3b84f3c9b7dd6fc32ddbec123cd4e225da9
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-02-24 13:09:36 +0100

    drugi commit

--- src/novaKlasa.java
+++ src/novaKlasa.java
@@ -2,5 +2,9 @@ public class novaKlasa {
     public static void main(String[] args) {
         System.out.println("YO! YO!");
         System.out.println("YO! YO!");
+
+        System.out.println("YO! YO!");
+
+        System.out.println("YO! YO!");
     }
 }
